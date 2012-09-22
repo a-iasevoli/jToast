88d0399b4793ee2589cd2a96eb2c20813648692f
middleman90 <iasevoli90@gmail.com>
2012-09-22 12:30:15 +0200

    #1

---
+++ jToast.js
@@ -0,0 +1,76 @@
+/*
+ * 
+ * varie callback
+ * linkabile
+ * toast con chiusura non automatica
+ * 
+ * 
+ */
+
+(function($) {
+	var toast = [];
+	var id = 0;
+	
+	removeToastAnimation = function(toast) {
+		toast.animate({
+			opacity: 0
+		}, 500, function(){
+			toast.parent().animate({
+				height: '0px'
+			}, 200, function(){
+				toast.parent().remove();
+			});
+		});
+	};
+	
+	removeToast = function(toast, duration) {
+		setTimeout(function() { removeToastAnimation(toast)}, duration);
+	};
+	
+	$('.toast .close').live('click', function(){
+		removeToastAnimation($(this).parent());
+	});
+	
+	$.fn.jToast = function(options) {
+		
+		 var settings = $.extend( {
+		      'type' : 'info',
+		      'text' : '',
+		      'duration': 8000
+		    }, options);
+		
+
+		if($('.toaster').length == 0) {
+			$('<div/>', {
+				'class' : 'toaster'
+			}).appendTo('body');
+			
+			$('.toaster').css({
+				bottom: '0px',
+				left: '0px'
+			});
+			
+		}
+		
+		toastContainer = $('<div/>', {
+			'class' : 'toast-container'
+		});
+		
+		toast = $('<div/>', {
+			'class' : 'toast alert-'+settings.type,
+			id : id,
+			html: settings.text
+		});
+		
+		close = $('<button/>', {
+			'class' : 'close',
+			text : 'x'
+		});
+		
+		toastContainer.html(toast).appendTo('.toaster');
+		toast.slideDown();
+		removeToast(toast, settings.duration);
+		id++;
+
+	};
+})(jQuery);
